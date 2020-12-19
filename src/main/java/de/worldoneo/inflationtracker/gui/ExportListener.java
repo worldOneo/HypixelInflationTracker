46e264f1f0a00d19faaa993b878f27f6ad325d12
worldOneo <62426504+worldOneo@users.noreply.github.com>
2020-12-19 11:44:07 +0100

    Added export as image Changed render order to render font on top.

--- src/main/java/de/worldoneo/inflationtracker/gui/ExportListener.java
+++ src/main/java/de/worldoneo/inflationtracker/gui/ExportListener.java
@@ -3,16 +3,19 @@ package de.worldoneo.inflationtracker.gui;
 import de.worldoneo.inflationtracker.InflationTracker;
 import de.worldoneo.inflationtracker.util.Exporter;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class ExportListener implements ActionListener {
     private JFrame jFrame;
     private InflationGraph inflationGraph;
+    public static final String[] IMAGE_TYPES = new String[]{"png", "jpg", "bmp"};
 
     public ExportListener(JFrame jFrame, InflationGraph inflationGraph) {
         this.jFrame = jFrame;
@@ -24,17 +27,31 @@ public class ExportListener implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         FileDialog fileDialog = new FileDialog(jFrame, "Export inflation data", FileDialog.SAVE);
         fileDialog.setVisible(true);
+
         String result = fileDialog.getFile();
         if (result == null) return;
         try {
             File file = new File(fileDialog.getDirectory() + result);
+            String absolutePath = file.getAbsolutePath();
             if (result.endsWith(".json")) {
-                InflationTracker.logger.info("Exporting inflation data to " + file.getAbsolutePath() + " as JSON");
+                InflationTracker.logger.info("Exporting inflation data to " + absolutePath + " as JSON");
                 Exporter.exportJSON(file, inflationGraph.getPointList(), true);
+                return;
             } else if (result.endsWith(".csv")) {
-                InflationTracker.logger.info("Exporting inflation data to " + file.getAbsolutePath() + " as CSV");
+                InflationTracker.logger.info("Exporting inflation data to " + absolutePath + " as CSV");
                 Exporter.exportCSV(file, inflationGraph.getPointList());
+                return;
             }
+            for (String imageType : IMAGE_TYPES) {
+                if (absolutePath.endsWith("." + imageType)) {
+                    InflationTracker.logger.info("Exporting inflation data to " + absolutePath + " as " + imageType.toUpperCase());
+                    BufferedImage bufferedImage = new BufferedImage(inflationGraph.getWidth(), inflationGraph.getHeight(), BufferedImage.TYPE_INT_RGB);
+                    inflationGraph.update(bufferedImage.getGraphics());
+                    ImageIO.write(bufferedImage, imageType, file);
+                    return;
+                }
+            }
+            JOptionPane.showMessageDialog(jFrame, "The file couldn't be exported as the given format!\nAvailable formats are: CSV,JSON,JPG,BMP,PNG");
         } catch (IOException exception) {
             InflationTracker.logger.error("Couldn't export file", exception);
         }
--- src/main/java/de/worldoneo/inflationtracker/gui/InflationGraph.java
+++ src/main/java/de/worldoneo/inflationtracker/gui/InflationGraph.java
@@ -3,6 +3,7 @@ package de.worldoneo.inflationtracker.gui;
 import de.worldoneo.inflationtracker.Config;
 import de.worldoneo.inflationtracker.InflationTracker;
 import de.worldoneo.inflationtracker.calculator.InflationCalculator;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +13,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.GeneralPath;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class InflationGraph extends JPanel implements ActionListener {
@@ -54,11 +56,11 @@ public class InflationGraph extends JPanel implements ActionListener {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
 
-        int height = getHeight();
+        int height = getHeight() - 10;
         double v = pMax - pMin;
 
         g2.setColor(Color.DARK_GRAY);
-        g2.fillRect(0, 0, getWidth(), height);
+        g2.fillRect(0, 0, getWidth(), getHeight());
 
         GeneralPath path = new GeneralPath();
         GeneralPath average = new GeneralPath();
@@ -66,7 +68,8 @@ public class InflationGraph extends JPanel implements ActionListener {
 
 
         double startY = height - ((1 - pMin) / v * height);
-        double xScale = getWidth() / (pointList.get(pointList.size() - 1).getTime() - (double) pointList.get(0).getTime());
+        int size = pointList.size();
+        double xScale = (getWidth() - 12) / (pointList.get(size - 1).getTime() - (double) pointList.get(0).getTime());
 
         path.moveTo(0, startY);
         average.moveTo(0, startY);
@@ -74,23 +77,41 @@ public class InflationGraph extends JPanel implements ActionListener {
 
         int incr = 0;
         double totalValue = 0;
+        List<Coordinate> normalCoordinates = new ArrayList<>(size);
+        List<Coordinate> avgCoordinates = new ArrayList<>(size);
         for (InflationCalculator.Point point : pointList) {
             incr++;
             totalValue += point.getValue();
             double x = (point.getTime() - pointList.get(0).getTime()) * xScale;
-            double y = height - ((point.getTime() - pMin) / v * height);
-            double avgY = height - (((totalValue / incr) - pMin) / v * height);
-            average.lineTo(x, avgY);
-            path.lineTo(x, y);
+            double y = height - ((point.getValue() - pMin) / v * height) + 10;
+            double avgY = height - (((totalValue / incr) - pMin) / v * height) + 10;
+
+            g2.setColor(Color.GRAY);
+            g2.drawLine((int) x, 0, (int) x, getHeight());
+
+            normalCoordinates.add(new Coordinate(x, y, point));
+            avgCoordinates.add(new Coordinate(x, avgY, new InflationCalculator.Point(0, (totalValue / incr))));
+        }
+
+        for (Coordinate coordinate : normalCoordinates) {
+            g2.setColor(Color.WHITE);
+            g2.drawString(String.format("%.2f", coordinate.getPoint().getValue()), (int) coordinate.getX() - 12, (int) coordinate.getY());
+            path.lineTo(coordinate.getX(), coordinate.getY());
+        }
+
+        for (Coordinate coordinate : avgCoordinates) {
+            g2.drawString(String.format("%.2f", coordinate.getPoint().getValue()), (int) coordinate.getX() - 12, (int) coordinate.getY());
+            average.lineTo(coordinate.getX(), coordinate.getY());
         }
 
         middle.lineTo(getWidth(), startY);
+        g2.setStroke(new BasicStroke(2));
+        g2.setColor(Color.RED);
+        g2.draw(middle);
         g2.setColor(Color.CYAN);
         g2.draw(path);
         g2.setColor(Color.GREEN);
         g2.draw(average);
-        g2.setColor(Color.RED);
-        g2.draw(middle);
     }
 
     @Override
@@ -100,5 +121,12 @@ public class InflationGraph extends JPanel implements ActionListener {
 
     public List<InflationCalculator.Point> getPointList() {
         return pointList;
+    }
+
+    @Data
+    public static class Coordinate {
+        private final double x;
+        private final double y;
+        private final InflationCalculator.Point point;
     }
 }
